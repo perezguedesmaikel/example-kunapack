7ce2f1472e5ed7a9566533baf78a80df719bfe7f
Lian Castellon <liancg@gmail.com>
2024-02-01 18:25:59 -0500

    Consulta de Aporte Familiar: Refactored the pages using the new procedure components.

--- pages/consulta-aporte-familiar-middle.vue
+++ pages/consulta-aporte-familiar-middle.vue
@@ -1,57 +1,55 @@
 <template>
-  <div role=" main" class="container"
-       aria-label="Consulta para saber si usted es el beneficiario del Aporte Familiar Permanente">
-    <header class="mb-6">
-      <DsTypography>
-        Servicio de información entregado por
-
-        <DsLink href="https://www.chileatiende.gob.cl/instituciones/AL005"
-           title="Página web del Instituto de Previsión Social IPS"
-           class="link">
-          Instituto de Previsión Social
-        </DsLink>
-      </DsTypography>
-
-      <DsTypography variant="h1">
-        ¿Desea saber si usted es beneficiario del Aporte Familiar Permanente?
-      </DsTypography>
-    </header>
-
-    <section role="form">
-      <DsAlert type="info">
-        <div class="columns is-mobile">
-          <div class="column">
-            No se cumple segunda validación. (AFPER)
-          </div>
-        </div>
-      </DsAlert>
-
-      <div class="cont-btn">
-        <DsButton color="tertiary"
-          title="Consultar otro RUN"
-          aria-labelledby="form_global"
-          class="button is-default">
-          Consultar otro RUT
-        </DsButton>
+  <ProcedurePage
+    title="¿Desea saber si usted es beneficiario del Aporte Familiar Permanente?"
+    ariaTitle="Consulta para saber si usted es el beneficiario del Aporte Familiar Permanente">
+    <DsAlert type="info">
+      <div>
+        No se cumple segunda validación. (AFPER)
       </div>
-    </section>
-  </div>
+    </DsAlert>
 
-  <hr />
+    <div class="cont-btn">
+      <DsButton color="tertiary"
+        title="Consultar otro RUN"
+        aria-labelledby="form_global">
+        Consultar otro RUT
+      </DsButton>
 
-  <div class="container pt-10">
-    <DsButton color="tertiary" :rounded="false"
-          title="Continuar al paso siguiente"
-          aria-labelledby="form_global"
-          class="button btn-ref is-default">
-          Continuar
-    </DsButton>
-  </div>
+      <DsButton color="tertiary"
+        title="Volver al paso anterior"
+        aria-labelledby="form_global"
+        @click="goBack">
+        <span>Volver</span>
+      </DsButton>
+    </div>
+
+    <hr />
+
+    <div class="container pt-10">
+      <DsButton color="tertiary" :rounded="false"
+        title="Continuar al paso siguiente"
+        aria-labelledby="form_global"
+        @click="goToNext">
+        Continuar
+      </DsButton>
+    </div>
+  </ProcedurePage>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import DsButton from '@perezguedesmaikel/chile-atiende-ui/components/DesignSystem/components/basic/button/DsButton.vue';
-import DsTypography from '@perezguedesmaikel/chile-atiende-ui/components/DesignSystem/components/basic/typography/DsTypography.vue';
-import DsLink from '@perezguedesmaikel/chile-atiende-ui/components/DesignSystem/components/navigation/link/DsLink.vue';
 import DsAlert from '@perezguedesmaikel/chile-atiende-ui/components/DesignSystem/components/basic/alert/DsAlert.vue';
+
+const goBack = () => {
+  navigateTo({
+    path: '/consulta-aporte-familiar-start',
+  });
+};
+
+const goToNext = () => {
+  navigateTo({
+    path: '/consulta-aporte-familiar-end',
+  });
+};
+
 </script>
--- pages/consulta-aporte-familiar-start.vue
+++ pages/consulta-aporte-familiar-start.vue
@@ -1,102 +1,46 @@
 <template>
-  <div class="container">
-    <div class="grid grid-cols-3 gap-2">
-      <main class="col-span-2"
-        aria-label="Consulta para saber si usted es el beneficiario del Aporte Familiar Permanente">
-        <header class="mb-6">
-          <DsTypography>
-            Servicio de información entregado por
-            <DsLink href="https://www.chileatiende.gob.cl/instituciones/AL005"
-              title="Página web del Instituto de Previsión Social IPS"
-              class="link">
-              Instituto de Previsión Social
-            </DsLink>
-          </DsTypography>
+  <ProcedurePage
+    title="¿Desea saber si usted es beneficiario del Aporte Familiar Permanente?"
+    ariaTitle="Consulta para saber si usted es el beneficiario del Aporte Familiar Permanente">
+    <form>
+      <div class="cont-form-sector">
+        <div class="cont-form-group">
+          <DsInput
+            v-model="model.rut"
+            label="Ingresa el RUT del beneficiario"
+            placeholder="Ejemplo: 12.345.678-9"
+            :error="rutErrors"
+            required />
+        </div>
 
-          <DsTypography variant="h1">
-            ¿Desea saber si usted es beneficiario del Aporte Familiar Permanente?
-          </DsTypography>
-        </header>
-
-        <section role="form">
-          <form>
-            <div class="cont-form-sector">
-              <div class="cont-form-group">
-                <DsInput
-                  v-model="model.rut"
-                  label="Ingresa el RUT del beneficiario"
-                  placeholder="Ejemplo: 12.345.678-9"
-                  :error="rutErrors"
-                  required />
-              </div>
-
-              <div>
-                <DsDatePicker
-                  v-model="model.date"
-                  labelText="Indica la fecha de nacimiento"
-                  :error="dateErrors"
-                  required  />
-              </div>
-            </div>
-
-            <div class="my-5">
-              <span>Los campos marcados con * son obligatorios</span>
-            </div>
-
-            <div class="flex justify-end cont-btn">
-              <DsButton color="primary" :rounded="false"
-                title="Continuar al paso siguiente"
-                @click="submitForm">
-                Consultar
-              </DsButton>
-            </div>
-          </form>
-        </section>
-      </main>
-
-      <div>
-        <div class="p-4 rounded-lg border border-neutral-300"
-             aria-labelledby="title-RRSS subtitle-RRSS">
-          <DsTypography variant="h3" id="title-RRSS">
-            ¿Tienes dudas con este trámite?
-          </DsTypography>
-
-          <DsTypography variant="p" id="subtitle-RRSS">
-            Comunícate con nosotros
-          </DsTypography>
-
-          <nav class="my-4 flex justify-cente">
-            <DsLink target="_blank" title="Ir a Twitter"
-                    class="flex items-center mb-5 text-lg text-primary"
-                    aria-label="Twitter">
-              <DsIcon name="twitter" prefix="lab" size="xlarge"/>
-            </DsLink>
-
-            <DsLink target="_blank" title="Ir a Facebook"
-                    class="flex items-center mb-5 text-lg text-primary"
-                    aria-label="Facebook">
-              <DsIcon name="facebook" prefix="lab" size="xlarge" />
-            </DsLink>
-
-            <DsLink target="_blank" title="Ir a Instagram"
-                    class="flex items-center mb-5 text-lg text-primary"
-                    aria-label="Instagram">
-              <DsIcon name="instagram" prefix="lab" size="xlarge" />
-            </DsLink>
-          </nav>
+        <div>
+          <DsDatePicker
+            v-model="model.date"
+            labelText="Indica la fecha de nacimiento"
+            :error="dateErrors"
+            required  />
         </div>
       </div>
-    </div>
-  </div>
+
+      <div class="my-5">
+        <span>Los campos marcados con * son obligatorios</span>
+      </div>
+
+      <div class="flex justify-end cont-btn">
+        <DsButton color="primary" :rounded="false"
+          title="Continuar al paso siguiente"
+          @click="submitForm">
+          Consultar
+        </DsButton>
+      </div>
+    </form>
+  </ProcedurePage>
 </template>
 
-<script setup type="ts">
+<script lang="ts" setup>
 import DsButton from '@perezguedesmaikel/chile-atiende-ui/components/DesignSystem/components/basic/button/DsButton.vue';
-import DsIcon from '@perezguedesmaikel/chile-atiende-ui/components/DesignSystem/components/basic/icon/DsIcon.vue';
-import DsTypography from '@perezguedesmaikel/chile-atiende-ui/components/DesignSystem/components/basic/typography/DsTypography.vue';
 import DsDatePicker from '@perezguedesmaikel/chile-atiende-ui/components/DesignSystem/components/form/datePicker/DsDatePicker.vue';
 import DsInput from '@perezguedesmaikel/chile-atiende-ui/components/DesignSystem/components/form/input/DsInput.vue';
-import DsLink from '@perezguedesmaikel/chile-atiende-ui/components/DesignSystem/components/navigation/link/DsLink.vue';
 import { useVuelidate } from '@vuelidate/core'
 import { required, helpers } from '@vuelidate/validators'
 
@@ -110,12 +54,12 @@ const submitForm = async () => {
 
   if (isValid) {
     navigateTo({
-      path: '/final-consulta',
+      path: '/consulta-aporte-familiar-middle',
     });
   }
 };
 
-const rutValidator = (value) => {
+const rutValidator = (value: string) => {
   if (!value) {
     return true;
   }
@@ -142,18 +86,18 @@ const dateErrors = computed(() => {
   return fieldErrors( v$.value.date);
 });
 
-const fieldErrors = (field) => {
-  return field.$errors.map( (error) => error.$message).join( '<br/>');
+const fieldErrors = (field: any) => {
+  return field.$errors.map( (error: any) => error.$message).join( '<br/>');
 };
 
-const rules = () => ({
+const rules = computed(() => ({
   rut: {
     required,
     isValidRut,
   },
 
   date: { required },
-});
+}));
 
 const v$ = useVuelidate( rules, model);
 </script>
